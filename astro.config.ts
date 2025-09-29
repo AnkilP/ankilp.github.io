5dd284c6cf175244738d5aaec47d14625a0746e9
AnkilP <ankil335@gmail.com>
2025-09-29 16:07:43 -0400

    fixed gtag

--- astro.config.ts
+++ astro.config.ts
@@ -9,19 +9,12 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [tailwind({
     applyBaseStyles: false,
-  }), react(), sitemap(), partytown({
-      config: {
-        // Add the "dataLayer.push" as a forwarding-event.
-        forward: ['dataLayer.push'],
-      },
-    })],
+  }), react(), sitemap()],
   markdown: {
     remarkPlugins: [
       remarkMath,
